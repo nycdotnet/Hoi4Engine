a73498aa5d7ff9632e041391f90b20f09444412f
Steve Ognibene <steve.ognibene@outlook.com>
2023-12-17 21:26:07 -0500

    improve tests

--- Hoi4EngineTests/InfantryEquipmentParsingTests.cs
+++ Hoi4EngineTests/InfantryEquipmentParsingTests.cs
@@ -40,27 +40,72 @@ namespace Hoi4EngineTests
                 { "steel", 2 }
             });
 
-            // Basic Infantry Equipment
+            {
+                // Basic Infantry Equipment
+                var basic = equipment.InfantryEquipment.Single(eq => eq.Name == "infantry_equipment_0");
+                basic.Defense.Should().Be(20); // copied from archetype
+                basic.ArmorValue.Should().Be(0); // copied from archetype
+                basic.Year.Should().Be(1918);
+            }
 
-            var basic = equipment.InfantryEquipment.Single(eq => eq.Name == "infantry_equipment_0");
-            basic.Defense.Should().Be(20); // copied from archetype
-            basic.ArmorValue.Should().Be(0); // copied from archetype
-            basic.Year.Should().Be(1918);
+            {
+                // Infantry Equipment I
+                var infantryEquipI = equipment.InfantryEquipment.Single(eq => eq.Name == "infantry_equipment_1");
+                infantryEquipI.Year.Should().Be(1936);
+                infantryEquipI.Priority.Should().Be(10);
+                infantryEquipI.VisualLevel.Should().Be(1);
+                infantryEquipI.Defense.Should().Be(22);
+                infantryEquipI.Breakthrough.Should().Be(3);
+                infantryEquipI.SoftAttack.Should().Be(6);
+                infantryEquipI.HardAttack.Should().Be(1);
+                infantryEquipI.Piercing.Should().Be(4);
+                infantryEquipI.AirAttack.Should().Be(0);
+                infantryEquipI.ArmorValue.Should().Be(0); // copied from archetype
+                infantryEquipI.BuildCostIC.Should().Be(0.5m);
+                infantryEquipI.Resources.Should().BeEquivalentTo(new Dictionary<string, int> {
+                    { "steel", 2 }
+                });
+            }
 
-            // Infantry Equipment I
+            {
+                // Infantry Equipment II
+                var infantryEquipII = equipment.InfantryEquipment.Single(eq => eq.Name == "infantry_equipment_2");
+                infantryEquipII.Year.Should().Be(1939);
+                infantryEquipII.Priority.Should().Be(10);
+                infantryEquipII.VisualLevel.Should().Be(2);
+                infantryEquipII.Defense.Should().Be(28);
+                infantryEquipII.Breakthrough.Should().Be(4);
+                infantryEquipII.SoftAttack.Should().Be(9);
+                infantryEquipII.HardAttack.Should().Be(1.5m);
+                infantryEquipII.Piercing.Should().Be(5);
+                infantryEquipII.AirAttack.Should().Be(0);
+                infantryEquipII.ArmorValue.Should().Be(0); // copied from archetype
+                infantryEquipII.BuildCostIC.Should().Be(0.58m);
+                infantryEquipII.Reliability.Should().Be(0.9m);
+                infantryEquipII.Resources.Should().BeEquivalentTo(new Dictionary<string, int> {
+                    { "steel", 3 }
+                });
+            }
 
-            var infantryEquipI = equipment.InfantryEquipment.Single(eq => eq.Name == "infantry_equipment_1");
-            infantryEquipI.Priority.Should().Be(10);
-            infantryEquipI.VisualLevel.Should().Be(1);
-            infantryEquipI.Defense.Should().Be(22);
-            infantryEquipI.Breakthrough.Should().Be(3);
-            infantryEquipI.SoftAttack.Should().Be(6);
-            infantryEquipI.HardAttack.Should().Be(1);
-            infantryEquipI.Piercing.Should().Be(4);
-            infantryEquipI.AirAttack.Should().Be(0);
-            infantryEquipI.ArmorValue.Should().Be(0); // copied from archetype
-            infantryEquipI.Year.Should().Be(1936);
-            infantryEquipI.BuildCostIC.Should().Be(0.5m);
+            {
+                // Infantry Equipment III
+                var infantryEquipIII = equipment.InfantryEquipment.Single(eq => eq.Name == "infantry_equipment_3");
+                infantryEquipIII.Year.Should().Be(1942);
+                infantryEquipIII.Priority.Should().Be(10);
+                infantryEquipIII.VisualLevel.Should().Be(3);
+                infantryEquipIII.Defense.Should().Be(34);
+                infantryEquipIII.Breakthrough.Should().Be(5);
+                infantryEquipIII.SoftAttack.Should().Be(12);
+                infantryEquipIII.HardAttack.Should().Be(2);
+                infantryEquipIII.Piercing.Should().Be(10);
+                infantryEquipIII.AirAttack.Should().Be(0);
+                infantryEquipIII.ArmorValue.Should().Be(0); // copied from archetype
+                infantryEquipIII.BuildCostIC.Should().Be(0.69m);
+                infantryEquipIII.Reliability.Should().Be(0.8m);
+                infantryEquipIII.Resources.Should().BeEquivalentTo(new Dictionary<string, int> {
+                    { "steel", 4 }
+                });
+            }
         }
     }
 }
